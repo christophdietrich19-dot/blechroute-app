cf99de1b58df555fdc9c14dff2356ae67b505049
christophdietrich19-dot <christophdietrich19@gmail.com>
2026-08-21 21:39:25 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
-import "./leather-v14.css";
+import "./leather-v15.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
